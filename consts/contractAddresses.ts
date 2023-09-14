db7fe9dff36d7f1eeb16207343e9655546b64e2e
Fawl3r <Fawl3r@gmail.com>
2023-09-14 09:47:52 -0500

    up

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -1,6 +1,6 @@
 export const nftDropContractAddress =
   "0xB8a33200A4a09d8c5F5313D6a19A9609B916479e";
 export const tokenContractAddress =
-  "0xD63Dc18E4B2B9c5c7B33ECcE94002A2F02319355";
+  "0x7175F2C267cEfB8cb57Ef1aE8711bD84B11e61E3";
 export const stakingContractAddress =
   "0x08cB279D19F9CFaEe9CB44b84c353660a1db1183";
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       activeChain={activeChain}
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+      clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
