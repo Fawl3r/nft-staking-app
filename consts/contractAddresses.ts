7d72d51b5edbd6b8d3b6b5e66671c8b842c64559
Fawl3r <Fawl3r@gmail.com>
2023-10-18 12:34:18 -0500

    up

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -4,3 +4,4 @@ export const tokenContractAddress =
   "0x886c4C3149983D4Dbe13e251c95230A61b89F78c";
 export const stakingContractAddress =
   "0x51EA6b1543340235806F6c08b7aDBE6bCf393Ced";
+export const lockdays = 7;
