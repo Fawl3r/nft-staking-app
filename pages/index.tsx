763abcd42890d7585c166ab2d5e9839f0aae3b07
Fawl3r <Fawl3r@gmail.com>
2023-10-18 13:44:56 -0500

    up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,39 @@
+import { ConnectWallet } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import styles from "../styles/Home.module.css";
-
+import styles from "../styles/Home1.module.css";
 const Home: NextPage = () => {
   const router = useRouter();
 
   return (
     <div className={styles.container}>
+      {/* Add ConnectWallet Button here */}
+      <div className={styles.connectWalletButton}>
+        <ConnectWallet />
+      </div>
+
       {/* Top Section */}
-      <h1 className={styles.h1}>thirdweb Deploy - Custom Staking Contract</h1>
+      <h1 className={styles.h1}>Gods Of Gaming - Limited Edition F3 Token Generator dApp</h1>
       <div className={styles.nftBoxGrid}>
         <div
           className={styles.optionSelectBox}
           role="button"
-          onClick={() => router.push(`/mint`)}
-        >
-          {/* Mint a new NFT */}
-          <Image src="/icons/drop.webp" alt="drop" width={64} height={64} />
-          <h2 className={styles.selectBoxTitle}>Mint a new NFT</h2>
-          <p className={styles.selectBoxDescription}>
-            Use the NFT Drop Contract to claim an NFT from the collection.
-          </p>
-        </div>
-
-        <div
-          className={styles.optionSelectBox}
-          role="button"
-          onClick={() => router.push(`/stake`)}
+          onClick={() => router.push("/stake")}
         >
           {/* Staking an NFT */}
-          <Image src="/icons/token.webp" alt="token" width={64} height={64} />
+          <Image
+            src="/icons/token.webp"
+            alt="token"
+            width={64}
+            height={64}
+            loading="lazy" // This line is not strictly necessary for Next.js
+          />
+
           <h2 className={styles.selectBoxTitle}>Stake Your NFTs</h2>
           <p className={styles.selectBoxDescription}>
-            Use the custom staking contract deployed via <b>thirdweb Deploy</b>{" "}
-            to stake your NFTs, and earn tokens from the <b>Token</b> contract.
+            Use the staking dApp to yield <b>F3 Tokens</b> from staking your
+            NFTs, and earn purchase items from the <b>Game Marketplace.</b>
           </p>
         </div>
       </div>
